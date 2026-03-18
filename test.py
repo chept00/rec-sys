6ec67158d0dfc92be00d9e5a2f2c4b28680b54b8
dorothy <dorothy.cheruiyot@mbsgroup.dev>
2026-03-18 13:28:22 +0100

    add file

--- test.py
+++ test.py
@@ -1,4 +1,9 @@
-def divide_num(a,b):
-    return a/b
+def divide_num(a, b):
+    if b == 0:
+        return "Error: Division by zero is not allowed."
+    else:
+        return a / b
 
-divide_num(12,0) #error
+# Example usage:
+result = divide_num(12, 0)
+print(result)  
